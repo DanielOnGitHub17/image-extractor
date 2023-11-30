c4cf63be1bf67ce86a2c020e695565be77f846d1
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2023-11-30 14:49:40 +0000

    wrote test well

--- test_here.py
+++ test_here.py
@@ -1,4 +1,6 @@
 import web_classes as w
-html = w.get_html("tester.html", 1)
-html_imgs = w.ImageFromHTML()
-imgs = html_imgs.feed(html)
+html = w.get_html("https://www.squarespace.com/")
+html_imgs = w.ImageFromHTML("https://www.squarespace.com/")
+imgs = tuple(html_imgs.feed(html))
+img = w.ImageGetter()
+img.start(html_imgs.url, imgs[0], "imgs/")
