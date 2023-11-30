3a6c9b80eca4e297d621726f032148e9e138fd18
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2023-11-30 14:59:09 +0000

    can't remember what

--- test_here.py
+++ test_here.py
@@ -2,5 +2,8 @@ import web_classes as w
 html = w.get_html("https://www.squarespace.com/")
 html_imgs = w.ImageFromHTML("https://www.squarespace.com/")
 imgs = tuple(html_imgs.feed(html))
+print(len(imgs), imgs[0])
 img = w.ImageGetter()
+
 img.start(html_imgs.url, imgs[0], "imgs/")
+print(img.name)
