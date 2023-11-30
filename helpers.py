bfc304a558f6a5d3ca2f22cb370ba2a027daeeb9
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2023-11-30 14:57:50 +0000

    implement split_url in helpers

--- helpers.py
+++ helpers.py
@@ -1,4 +1,6 @@
 import os
+import urllib.parse as parse
+
 def check_some(book, *texts):
     """This function returns true if """
     return bool(sum(value in book for value in texts))
@@ -11,6 +13,18 @@ def str_attr(attrs):
     replace colons by equals
     """
     return ' '.join(f"{name}=\"{value}\"" for name, value in attrs)
+
+def split_url(website, src):
+    #use urllib.parse to query url given
+    full_src = parse.urlparse(src)
+    website = parse.urlparse(website)
+    if not full_src.netloc:
+        src_path = full_src.path
+        #turn 'daniel.jpg' to 'danielongithub17.github.io/daniel.jpg'
+        src = f"{website.netloc}{'' if src_path.startswith('/') else '/'}{src_path}"
+    if not full_src.scheme:
+        src = f"{website.scheme}://{src}"
+    return src
 
 def find_right_name(name, folder):
     name_hold = name
