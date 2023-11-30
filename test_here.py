b44a2dc860a267cf9e017b357d92f7f1b3314d7a
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2023-11-30 20:28:48 +0000

    decided to write each test as functions

--- test_here.py
+++ test_here.py
@@ -1,9 +1,17 @@
 import web_classes as w
-html = w.get_html("https://www.squarespace.com/")
-html_imgs = w.ImageFromHTML("https://www.squarespace.com/")
-imgs = tuple(html_imgs.feed(html))
-print(len(imgs), imgs[0])
-img = w.ImageGetter()
+#listed in reversed chronological order
 
-img.start(html_imgs.url, imgs[0], "imgs/")
-print(img.name)
+def test_css_class():
+    pass
+
+
+def test_img_classes():
+    html = w.get_html("https://www.squarespace.com/")
+    html_imgs = w.ImageFromHTML("https://www.squarespace.com/")
+    imgs = tuple(html_imgs.feed(html))
+    print(len(imgs), imgs[0])
+    img = w.ImageGetter()
+
+    img.start(html_imgs.url, imgs[0], "imgs/")
+    print(img.name)
+test_img_classes()
