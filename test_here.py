18de2bc3d65a3510647d725637bb3ca539da8432
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2023-12-06 01:21:39 +0000

    implemented parse_css text (works!) class later

--- test_here.py
+++ test_here.py
@@ -1,10 +1,34 @@
 import web_classes as w
+import os
 
 from importlib import reload
 #listed in reversed chronological order
-
+cls = lambda: exec("os.system('clear')")
 def parse_css():
-    pass
+    # remember to add 'data' url support
+    sample_style = r"""border-radius: fine;
+    balablu: english'
+    background: url("styles.css");
+@import url("hi.css");
+background-image: url("tellme.jpg")
+;
+    color: blue;
+background: left right center 10% blue url("assets/tellme.jpg");
+"""
+    # used +1 to indicate if sth is not -1
+    global urls
+    urls = set()
+    found_url = sample_style.find("url")
+    while found_url+1:
+        found_closing_brackets = sample_style.find(')', found_url)
+        if found_closing_brackets + 1:
+            #scrape the url out and get the src inside the bracket
+            url = sample_style[found_url:found_closing_brackets].strip("\'\"url( ")
+            urls.add(url)
+        # now find the next one
+        found_url = sample_style.find("url", found_closing_brackets)
+    return urls
+
 
 def test_css_class():
     pass
