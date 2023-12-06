510304220cb762e75e073f365116f0d8c04ecde5
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2023-12-06 01:29:52 +0000

    nothing doing

--- test_here.py
+++ test_here.py
@@ -15,8 +15,7 @@ background-image: url("tellme.jpg")
     color: blue;
 background: left right center 10% blue url("assets/tellme.jpg");
 """
-    # used +1 to indicate if sth is not -1
-    global urls
+    # used +1 to indicate if sth is not -1j 
     urls = set()
     found_url = sample_style.find("url")
     while found_url+1:
