867d2796353ee5821509e0c9f1472a515a752c2c
Daniel Enesi <enesidaniel.120064@gmail.com>
2025-12-27 20:31:28 -0600

    start helper for user code / command line interface

---
+++ img_extract.py
@@ -0,0 +1,15 @@
+"""Import this for image extraction"""
+from typing import Set
+from web_classes import ImageFromHTML, ImageGetter, SVGMaker
+
+# functions
+from helpers import get_web_text
+
+
+def extract_images(url: str, destination: str = "None", is_file: bool=False) -> Set[str]:
+    url = input("Enter web/file url:")
+
+    html_text = get_web_text(url, is_file=is_file)
+    img_srcs, svg_texts = ImageFromHTML(url).feed(html_text, url)
+
+    return img_srcs or svg_texts
