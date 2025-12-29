0bf8b95b398a12b7fa5e037bdf9f9ddaf1381624
Daniel Enesi <enesidaniel.120064@gmail.com>
2025-12-29 01:28:55 -0600

    Complete img_extract for use across UI, command line, and library imports

--- img_extract.py
+++ img_extract.py
@@ -1,14 +1,43 @@
 """Import this for image extraction"""
+
+import os
+
+from helpers import get_web_text
 from web_classes import ImageFromHTML, ImageGetter, SVGMaker
 
-# functions
-from helpers import get_web_text
+build_image = ImageGetter().start
+build_svg = SVGMaker().start
 
 
-def extract_images(url: str, destination: str = "None", is_file: bool=False) -> set[str]:
-    url = input("Enter web/file url:")
-
+def extract_images(url: str, is_file: bool = False) -> dict[str, set[str]]:
     html_text = get_web_text(url, is_file=is_file)
     img_srcs, svg_texts = ImageFromHTML(url).feed(html_text, url)
+    ret = {"img_srcs": img_srcs, "svg_texts": svg_texts}
+    return ret
 
-    return img_srcs or svg_texts
+
+def download_images(
+    url: str, destination: str, img_srcs: set[str] = set(), svg_texts: set[str] = set()
+) -> None:
+    """Make this track number of successful downloads.
+    Like img_srcs: 3, 6 (for 3 out of 6), svg_text: like img_srcs
+    """
+    if not all((destination, img_srcs, svg_texts)): return
+    cwd = os.getcwd()
+    os.chdir(destination)
+    # download images
+    for img_src in img_srcs:
+        try:
+            build_image(url, img_src, destination)
+        except Exception as error:
+            print(error, img_src, sep="\n")
+
+    # download svgs
+    for svg_text in svg_texts:
+        try:
+            build_svg(svg_text, destination)
+        except Exception as error:
+            print(error)
+
+    os.startfile(destination)
+    os.chdir(cwd)
